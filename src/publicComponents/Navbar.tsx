eb94ed531dc88fe6e194c08c60feb4d94c2a25e1
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-07-22 23:08:36 +0900

    Feat: Navbar 안의 항목 클릭 시 원하는 페이지로 라우팅되는 기능 추가

--- src/publicComponents/Navbar.tsx
+++ src/publicComponents/Navbar.tsx
@@ -1,14 +1,15 @@
 import styles from "./Navbar.module.css";
 import NavbarList from "../publicComponents/NavbarList";
 
+// navigateTo에 string 타입으로 이동할 라우트 경로를 적으면 그 페이지로 이동
 function Navbar() {
   return (
     <ul className={styles.navbar}>
-      <NavbarList title="공항" />
-      <NavbarList title="환율" />
-      <NavbarList title="내 티켓" />
-      <NavbarList title="공항패션" />
-      <NavbarList title="마이페이지" />
+      <NavbarList title="공항" navigateTo="/" />
+      <NavbarList title="환율" navigateTo="#" />
+      <NavbarList title="내 티켓" navigateTo="#" />
+      <NavbarList title="공항패션" navigateTo="#" />
+      <NavbarList title="마이페이지" navigateTo="#" />
     </ul>
   );
 }
--- src/publicComponents/NavbarList.tsx
+++ src/publicComponents/NavbarList.tsx
@@ -2,13 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 type NavbarListProps = {
   title: string;
+  navigateTo: string;
 };
 
-function NavbarList({ title }: NavbarListProps) {
+function NavbarList({ title, navigateTo }: NavbarListProps) {
   const navbarNavigator = useNavigate();
 
   return (
-    <div onClick={() => navbarNavigator("#")}>
+    <div onClick={() => navbarNavigator(navigateTo)}>
       <li>
         <p>{title}</p>
       </li>
