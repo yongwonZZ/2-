44560783bf69b94ba92923c3a22828148dd65f4e
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-07-22 23:56:30 +0900

    Fix: App.tsx, index.tsx 주석 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "./mainPage/MainPage";
 import AirlineSearchPage from "./airlinePage/AirlineSearchPage";
 
+// 라우팅 설명
+// Routes 하위의 Route 컴포넌트: 다양한 URL 경로에 대한 라우트 정의
+// Header, Navbar 컴포넌트: 각각의 페이지 안에 속하도록 구성
 function App() {
   return (
     <div className="App">
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// BrowserRouter를 사용하여 App 컴포넌트에서 라우팅 기능 활성화
 root.render(
   <React.StrictMode>
     <BrowserRouter>
