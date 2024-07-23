617d659e29bdd20d34471935e281baa3e5e6d463
lim12749 <50003828+lim12749@users.noreply.github.com>
2024-07-23 16:38:00 +0900

    에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/airline-search" element={<AirlineSearchPage />} />
+
       </Routes>
     </div>
   );
---
+++ src/boardingPass/BoardingPass.tsx
@@ -0,0 +1,10 @@
+import  { useNavigate } from "react-router-dom";
+
+const BoardingPass = () =>{
+    return (
+        <>
+
+        </>
+    );
+}
+export default BoardingPass;
