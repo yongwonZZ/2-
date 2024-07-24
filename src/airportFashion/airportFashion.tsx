c7c62a28938c5693adc97ff6518b1e85e683d314
J-yun-ji <qety_1356@naver.com>
2024-07-24 21:38:55 +0900

    style: 게시글 UI

--- src/airportFashion/airportFashion.tsx
+++ src/airportFashion/airportFashion.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import "./airportFashion.css";
-import UploadIcon from "./images/pen-upload.svg";
+import { PiPencilSimpleLine } from "react-icons/pi";
+import LookFilter from "./airportFashionComponents/LookFilter";
 import FashionImagesItem from "./airportFashionComponents/FashionImagesItem";
+import LookSort from "./airportFashionComponents/LookSort";
 
-function AirportFashion() {
+const AirportFashion: React.FC = () => {
   return (
     <div>
       <h1>공항 패션</h1>
       <hr />
-      <div className='look-button-container'>
-        <button>캐주얼</button>
-        <button>스트릿</button>
-        <button>빈티지</button>
-        <button>시크</button>
-        <button>키치</button>
-        <button>??</button>
-        <button>??</button>
-      </div>
+      <LookFilter />
+      <LookSort />
       <div className='fashion-items-container'>
         <FashionImagesItem />
         <FashionImagesItem />
@@ -24,11 +19,11 @@ function AirportFashion() {
       </div>
       <div className='upload-button'>
         <button>
-          <img src={UploadIcon} alt='Upload Icon' />
+          <PiPencilSimpleLine size={20} />
         </button>
       </div>
     </div>
   );
-}
+};
 
 export default AirportFashion;
--- src/airportFashion/airportFashionComponents/FashionImagesItem.tsx
+++ src/airportFashion/airportFashionComponents/FashionImagesItem.tsx
@@ -1,35 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../airportFashion.css";
+import { fashionImageUrls } from "./fashionImageUrls";
 
-function FashionImagesItem() {
+const FashionImagesItem: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleImageClick = (imageUrl: string) => {
+    navigate("/lookDetails", { state: { imageUrl } });
+  };
   return (
     <div className='fashion-img-container'>
-      <img
-        src='https://cdn.wedding21.co.kr/news/photo/202307/259619_124101_5712.jpg'
-        alt='Fashion-Image'
-      />
-      <img
-        src='https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/09/20/38ca715c-6500-4939-86c9-6271c5d73cab.jpg'
-        alt='Fashion-Image'
-      />
-      <img
-        src='https://image.xportsnews.com/contents/images/upload/article/2022/0912/mb_1662936276431384.jpg'
-        alt='Fashion-Image'
-      />
-      <img
-        src='https://biz.chosun.com/resizer/tSykqxsCoxz7dPYOg7vgtNESh-U=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosunbiz/TUKSM5GGZAP36MSBNSOIFQPKEE.jpg'
-        alt='Fashion-Image'
-      />
-      <img
-        src='https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/03/02/29KdgdFZy7fU637818472773830903.jpg'
-        alt='Fashion-Image'
-      />
-      <img
-        src='https://www.elle.co.kr/resources_old/online/org_online_image/el/2fcc73c3-8cfd-4d86-ad18-a0da5d56e2b6.jpg'
-        alt='Fashion-Image'
-      />
+      {fashionImageUrls.map((url, index) => (
+        <img
+          key={index}
+          src={url}
+          alt={`Fashion-Image${index}`}
+          onClick={() => handleImageClick(url)}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default FashionImagesItem;
