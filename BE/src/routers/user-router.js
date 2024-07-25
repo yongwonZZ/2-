65970a835057abf7d7138fb48a4323e748196ee2
yongwonZZ <shin082508@naver.com>
2024-07-25 23:51:36 +0900

    수정4일차

--- BE/src/routers/user-router.js
+++ BE/src/routers/user-router.js
@@ -133,3 +133,19 @@ userRouter.delete('/me/:userId', loginRequired, async (req, res, next) => {
 });
 
 export { userRouter };
+
+// const express = require('express');
+// const { getUserList, updateUser, deleteUser, getUser, resignUser } = require('../services/userServices')
+// const permission = require('../middlewares/permission'); // 유저인증 & 권한 체크
+// const router = express.Router();
+
+// router.get('/mypage', permission('user'), getUser) // 회원 정보 조회
+// router.put('/mypage/:id', permission('user'), updateUser) // 회원 정보 수정
+// router.delete('/mypage/:id', permission('user'), resignUser) // 회원 삭제(탈퇴)
+
+// router.get('/admin', permission('admin'), getUserList) // 전체 회원 목록 조회
+// router.get('/admin/:id', permission('admin'), getUser) // 회원 정보 조회
+// router.put('/admin/:id', permission('admin'), updateUser) // 회원 정보 수정
+// router.delete('/admin/:id', permission('admin'), deleteUser) // 회원 삭제
+
+// module.exports = router;
