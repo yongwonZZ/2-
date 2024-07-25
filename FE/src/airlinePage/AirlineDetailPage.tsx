7a514b7248a9fa0e7ec17c4d6ab6f4eed407d874
yuzunsang <111577841+yuzunsang@users.noreply.github.com>
2024-07-25 16:53:26 +0900

    Feat: 항공편 상세 페이지 UI 완성

--- FE/src/airlinePage/AirlineDetailPage.tsx
+++ FE/src/airlinePage/AirlineDetailPage.tsx
@@ -1,7 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useFetchAirlineData } from "../airlineSearchComponents/hooks/useFetchAirlineData";
+import styles from "./AirlineDetailPage.module.css";
+import stylesEx from "../airlineSearchComponents/AirlineSearchResultCard.module.css";
+import Header from "../publicComponents/Header";
+import { formatTime } from "../airlineSearchComponents/utils/formatTime";
+import { FaChevronLeft } from "react-icons/fa";
+import { IoMdShare } from "react-icons/io";
+import { IoIosAirplane } from "react-icons/io";
 
 function AirlineDetailPage() {
+  const navigate = useNavigate();
+
   /** flightId 소문자 형식 */
   let { id } = useParams();
   /** flightId 대문자 형식으로 */
@@ -13,7 +22,66 @@ function AirlineDetailPage() {
   /** 자료에서 flightId에 해당하는 특정 항공편 하나 찾기 */
   const detailData = data?.find(({ flightId }) => flightId === id);
 
-  return <h1>{detailData?.estimatedDateTime}</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <Header
+        leftContent={
+          <FaChevronLeft
+            className={styles["icon-prev"]}
+            onClick={() => navigate(-1)}
+          />
+        }
+        centerContent={detailData?.flightId}
+        rightContent={<IoMdShare className={styles["icon-share"]} />}
+      />
+      <div className={styles.card}>
+        <div className={styles.section}>
+          <span>{detailData?.airline}</span>
+          <span>{detailData?.flightId}</span>
+        </div>
+        <div className={styles.section}>
+          <span>운항현황</span>
+          <span className={stylesEx["section-blue"]}>{detailData?.remark}</span>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.section}>
+          <span>{detailData?.airport}</span>
+          <span>{detailData?.airportCode}</span>
+        </div>
+        <IoIosAirplane className={styles["icon-departure"]} />
+        <div className={styles.section}>
+          <span>인천(서울)</span>
+          <span>ICN</span>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <span>터미널</span>
+        <span>{detailData?.terminalid}</span>
+      </div>
+      <div className={styles.card}>
+        <span>수하물수취대</span>
+        <span>{detailData?.carousel}</span>
+      </div>
+      <div className={styles.card}>
+        <span>출구</span>
+        <span>{detailData?.exitnumber}</span>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.section}>
+          <span>도착예정시간</span>
+          <span>도착변경시간</span>
+        </div>
+        <div className={styles.section}>
+          <span className={stylesEx["section-strikethrough"]}>
+            {formatTime(detailData!.scheduleDateTime)}
+          </span>
+          <span>{formatTime(detailData!.estimatedDateTime)}</span>
+        </div>
+      </div>
+      <button className={styles.btn}>내 항공편 등록하기</button>
+    </div>
+  );
 }
 
 export default AirlineDetailPage;
