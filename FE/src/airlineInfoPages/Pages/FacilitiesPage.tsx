17848089f86f730db6e3e8118f3773912e72b4e9
G-jjunny <rudwns9551@naver.com>
2024-07-27 17:18:43 +0900

    facility search

--- FE/src/airlineInfoPages/Pages/FacilitiesPage.tsx
+++ FE/src/airlineInfoPages/Pages/FacilitiesPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Header from "../../publicComponents/Header";
 import FacilitiesItem from "../airlineComponents/FacilitiesItem";
-import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronLeft, FaSearch } from "react-icons/fa";
 import { fetchFacilitiesData } from "../getInfoData/getFacilitiesData";
 import { Link } from "react-router-dom";
 
@@ -16,7 +16,9 @@ interface Facility {
 
 const FacilitiesPage: React.FC = () => {
   const [facility, setFacility] = useState<Facility[]>([]);
-  const type = "&facility_nm=&numOfRows=50&pageNo=1&type=json";
+  const [searchToggle, setSearchToggle] = useState<boolean>(false);
+  const [searchInput, setSearchInput] = useState<string>("");
+  const type = "&facility_nm=&numOfRows=200&pageNo=1&type=json";
 
   /** 편의시설 데이터를 state에 저장합니다 */
   useEffect(() => {
@@ -35,28 +37,59 @@ const FacilitiesPage: React.FC = () => {
     <div className="container">
       <Header
         leftContent={
-          <div className="facility-header">
-            <Link to={"/"}>
-              <FaChevronLeft style={{ fontSize: "22px", cursor: "pointer" }} />
-            </Link>
-            편의시설
-          </div>
+          searchToggle ? (
+            <div className="facility-header">
+              <FaChevronLeft
+                style={{ fontSize: "22px", cursor: "pointer" }}
+                onClick={() => setSearchToggle(false)}
+              />
+              <input
+                value={searchInput}
+                type="text"
+                className="search-input"
+                placeholder="검색어를 입력해 주세요"
+              />
+            </div>
+          ) : (
+            <div className="facility-header">
+              <Link to={"/"}>
+                <FaChevronLeft
+                  style={{ fontSize: "22px", cursor: "pointer" }}
+                />
+              </Link>
+              편의시설
+            </div>
+          )
+        }
+        rightContent={
+          <FaSearch
+            style={{ fontSize: "22px" }}
+            onClick={() => setSearchToggle(true)}
+          />
         }
       />
-      {facility && facility.length > 0 ? (
-        facility.map((item, index) => (
-          <FacilitiesItem
-            key={index}
-            name={item.entrpskoreannm}
-            service={item.trtmntprdlstkoreannm}
-            location={item.lckoreannm}
-            arrOrDep={item.arrordep}
-            serviceTime={item.servicetime}
-            tel={item.tel}
-          />
-        ))
+      {searchToggle ? (
+        <></>
       ) : (
-        <p>편의시설 데이터를 불러오는 중입니다...</p>
+        <>
+          {facility && facility.length > 0 ? (
+            facility.map((item, index) => (
+              <FacilitiesItem
+                key={index}
+                name={item.entrpskoreannm}
+                service={item.trtmntprdlstkoreannm}
+                location={item.lckoreannm}
+                arrOrDep={item.arrordep}
+                serviceTime={item.servicetime}
+                tel={item.tel}
+              />
+            ))
+          ) : (
+            <div className="loading-data">
+              <p>편의시설 데이터를 불러오는 중입니다...</p>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
