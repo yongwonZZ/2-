3d3cd2c698c184e23224e1b70cd179ae41fe05d8
G-jjunny <rudwns9551@naver.com>
2024-07-27 18:04:38 +0900

    search component

--- FE/src/airlineInfoPages/Pages/FacilitiesPage.tsx
+++ FE/src/airlineInfoPages/Pages/FacilitiesPage.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+  useMemo,
+} from "react";
 import Header from "../../publicComponents/Header";
 import FacilitiesItem from "../airlineComponents/FacilitiesItem";
 import { FaChevronLeft, FaSearch } from "react-icons/fa";
@@ -18,24 +24,76 @@ const FacilitiesPage: React.FC = () => {
   const [facility, setFacility] = useState<Facility[]>([]);
   const [searchToggle, setSearchToggle] = useState<boolean>(false);
   const [searchInput, setSearchInput] = useState<string>("");
-  const type = "&facility_nm=&numOfRows=200&pageNo=1&type=json";
+  const type = "&facility_nm=&numOfRows=300&pageNo=1&type=json";
+
+  const fetchData = useCallback(async () => {
+    try {
+      const faciData = await fetchFacilitiesData(type);
+      setFacility(faciData.response.body.items);
+    } catch (error) {
+      console.error("Failed to fetch parking data", error);
+    }
+  }, []);
 
   /** 편의시설 데이터를 state에 저장합니다 */
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const faciData = await fetchFacilitiesData(type);
-        // console.log("faci data : ", faciData);
-        setFacility(faciData.response.body.items);
-      } catch (error) {
-        console.error("Failed to fetch parking data", error);
-      }
-    };
     fetchData();
-  }, []);
+  }, [fetchData]);
+
+  /** 입력된 검색어를 기반으로 데이터 필터링 */
+  const filteredFacilities = useMemo(
+    () =>
+      facility.filter(
+        (item) =>
+          item.entrpskoreannm
+            .toLowerCase()
+            .includes(searchInput.toLowerCase()) ||
+          item.trtmntprdlstkoreannm
+            .toLowerCase()
+            .includes(searchInput.toLowerCase())
+      ),
+    [facility, searchInput]
+  );
+
+  const handleSearchInputChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchInput(e.target.value);
+    },
+    []
+  );
+
   return (
-    <div className="container">
-      <Header
+    <>
+      <div className="facility-header">
+        {searchToggle ? (
+          <div className="fac-header-left">
+            <FaChevronLeft
+              style={{ fontSize: "22px", cursor: "pointer" }}
+              onClick={() => setSearchToggle(false)}
+            />
+            <input
+              value={searchInput}
+              type="text"
+              className="search-input"
+              placeholder="검색어를 입력해 주세요"
+              onChange={handleSearchInputChange}
+            />
+          </div>
+        ) : (
+          <div className="fac-header-left">
+            <Link to={"/"}>
+              <FaChevronLeft style={{ fontSize: "22px", cursor: "pointer" }} />
+            </Link>
+            편의시설
+          </div>
+        )}
+        <FaSearch
+          style={{ fontSize: "22px" }}
+          onClick={() => setSearchToggle(true)}
+        />
+      </div>
+      <div className="container faci-container">
+        {/* <Header
         leftContent={
           searchToggle ? (
             <div className="facility-header">
@@ -48,6 +106,7 @@ const FacilitiesPage: React.FC = () => {
                 type="text"
                 className="search-input"
                 placeholder="검색어를 입력해 주세요"
+                onChange={handleSearchInputChange}
               />
             </div>
           ) : (
@@ -67,13 +126,10 @@ const FacilitiesPage: React.FC = () => {
             onClick={() => setSearchToggle(true)}
           />
         }
-      />
-      {searchToggle ? (
-        <></>
-      ) : (
-        <>
-          {facility && facility.length > 0 ? (
-            facility.map((item, index) => (
+      /> */}
+        {searchToggle ? (
+          <>
+            {filteredFacilities.map((item, index) => (
               <FacilitiesItem
                 key={index}
                 name={item.entrpskoreannm}
@@ -83,15 +139,31 @@ const FacilitiesPage: React.FC = () => {
                 serviceTime={item.servicetime}
                 tel={item.tel}
               />
-            ))
-          ) : (
-            <div className="loading-data">
-              <p>편의시설 데이터를 불러오는 중입니다...</p>
-            </div>
-          )}
-        </>
-      )}
-    </div>
+            ))}
+          </>
+        ) : (
+          <>
+            {facility && facility.length > 0 ? (
+              facility.map((item, index) => (
+                <FacilitiesItem
+                  key={index}
+                  name={item.entrpskoreannm}
+                  service={item.trtmntprdlstkoreannm}
+                  location={item.lckoreannm}
+                  arrOrDep={item.arrordep}
+                  serviceTime={item.servicetime}
+                  tel={item.tel}
+                />
+              ))
+            ) : (
+              <div className="loading-data">
+                <p>편의시설 데이터를 불러오는 중입니다...</p>
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
