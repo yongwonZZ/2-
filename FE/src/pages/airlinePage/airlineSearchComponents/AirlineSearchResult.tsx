bef35109bed60c0a5b0961813c3b3f06afebc9a1
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-07-30 20:08:27 +0900

    Feat: 항공편 검색 페이지 유저 메시지 개선

--- FE/src/pages/airlinePage/airlineSearchComponents/AirlineSearchResult.tsx
+++ FE/src/pages/airlinePage/airlineSearchComponents/AirlineSearchResult.tsx
@@ -13,22 +13,34 @@ function AirlineSearchResult({
   isLoading,
   error,
 }: AirlineSearchResultProps) {
+  const filteredData = data
+    ?.filter(
+      ({ estimatedDateTime }) =>
+        Number(estimatedDateTime) >= Number(currentFormatTime()) - 30
+    )
+    .slice(0, 10);
+
   return (
     <div>
       {isLoading && <div>데이터를 불러오는 중이에요..!</div>}
-      {error && <div>에러: {error.message}</div>}
+      {error && (
+        <div>
+          <p>데이터를 가져오는 데 실패했어요...</p>
+          <p>에러 메시지: {error.message}</p>
+        </div>
+      )}
       {typeof data === "undefined" ? (
         <div>검색어를 입력해주세요!</div>
+      ) : filteredData?.length === 0 ? (
+        <div>
+          <p>앗..! 검색 결과가 없습니다.</p>
+          <p>다른 검색어를 입력하거나</p>
+          <p>우측 상단의 검색어 필터를 확인해 보세요.</p>
+        </div>
       ) : (
-        data
-          ?.filter(
-            ({ estimatedDateTime }) =>
-              Number(estimatedDateTime) >= Number(currentFormatTime()) - 30
-          )
-          .slice(0, 10)
-          .map((item: searchResultType, index: number) => (
-            <AirlineSearchResultCard key={index} item={item} />
-          ))
+        filteredData?.map((item: searchResultType, index: number) => (
+          <AirlineSearchResultCard key={index} item={item} />
+        ))
       )}
     </div>
   );
