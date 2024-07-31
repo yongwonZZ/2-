85d51f51e0413db6225fa845e2ad5d62e2a7fc2c
G-jjunny <rudwns9551@naver.com>
2024-07-31 18:35:42 +0900

    1 last

--- FE/src/ExchangeRatePage/ExchangeRatePage.tsx
+++ FE/src/ExchangeRatePage/ExchangeRatePage.tsx
@@ -3,10 +3,8 @@ import ExRateHeaderItem from "./Components/ExRateHeaderItem";
 import "./Styles/ExchangeRatePage.css";
 import SelectedCountry from "./Components/SelectedCountry";
 import InputAmount from "./Components/InputAmount";
-import { fetchExchangeRate } from "./getExchangeRates";
+import { fetchExchangeRate, fetchCountryImage } from "./getExchangeRates";
 import Header from "../publicComponents/Header";
-import Navbar from "../publicComponents/Navbar";
-import { Link } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
 
 interface ExchangeRate {
@@ -21,22 +19,23 @@ const ExchangeRatePage = () => {
   const [baseCountry, setBaseCountry] = useState<ExchangeRate | null>(null);
   const [targetCountry, setTargetCountry] = useState<ExchangeRate | null>(null);
   const [isToggle, setIsToggle] = useState<boolean>(false);
+  const [isBase, setIsBase] = useState<boolean>(false);
+  const [countryImages, setCountryImages] = useState<{ [key: string]: string }>(
+    {}
+  );
 
-  // 기본 통화 목록
   const defaultCurrencies = ["USD", "JPY(100)", "EUR", "CNH"];
 
-  /** 하루 특정 시간에 따라 api 업데이트 */
-  /** 현재 시간을 반환 */
   const getCurrentTime = () => {
     const now = new Date();
     const year = String(now.getFullYear());
-    const month = String(now.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더함
+    const month = String(now.getMonth() + 1).padStart(2, "0");
     const date = String(now.getDate()).padStart(2, "0");
-    return `${year}${month}${date}`;
+    return `${year}.${month}.${date}`;
   };
-  const searchdate = getCurrentTime(); // 검색요청 날짜
 
-  const type = "AP01"; // 환율 데이터 요청타입 AP01 = json
+  const searchdate = getCurrentTime().split(".").join("");
+  const type = "AP01";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,71 +43,96 @@ const ExchangeRatePage = () => {
         const rates = await fetchExchangeRate(searchdate, type);
         setExchangeRates(rates);
 
-        // KRW와 USD의 환율 데이터를 설정 => default state
         const krwRate = rates.find((rate) => rate.cur_unit === "KRW");
         const usdRate = rates.find((rate) => rate.cur_unit === "USD");
 
         if (krwRate) setBaseCountry(krwRate);
         if (usdRate) setTargetCountry(usdRate);
+
+        // 국가 이미지 불러오기
+        for (const rate of rates) {
+          const countryName = rate.cur_nm.split(" ")[0];
+          const flagUrl = await fetchCountryImage(countryName);
+          setCountryImages((prevImages) => ({
+            ...prevImages,
+            [rate.cur_unit]: flagUrl,
+          }));
+        }
       } catch (error) {
         console.error("Failed to fetch exchange rates", error);
       }
     };
     fetchData();
-  }, []);
+  }, [searchdate, type]);
 
-  useEffect(() => {
-    console.log(exchangeRates);
-  }, [exchangeRates]);
-
-  /**기본 통화 목록에 있는 환율만 필터링 */
   const defaultRates = useMemo(
     () =>
       exchangeRates.filter((rate) => defaultCurrencies.includes(rate.cur_unit)),
     [exchangeRates]
   );
 
-  /** baseCountry와 targetCountry를 스왑합니다 */
   const handleSwapCountries = useCallback(() => {
     setBaseCountry(targetCountry);
     setTargetCountry(baseCountry);
   }, [baseCountry, targetCountry]);
 
+  const handleSelectCountry = useCallback(
+    (item: ExchangeRate) => {
+      if (isBase) {
+        setBaseCountry(item);
+      } else {
+        setTargetCountry(item);
+      }
+      setIsToggle(false);
+    },
+    [isBase]
+  );
+
   return isToggle ? (
-    <>
-      <div className="container">
-        <Header
-          leftContent={
-            <div className="exchange-header">
-              <FaChevronLeft
-                style={{ fontSize: "22px" }}
-                onClick={() => setIsToggle(false)}
-              />
-              통화
-            </div>
-          }
-        />
-        <div className="country-list">
-          {exchangeRates?.map((item, index) => (
-            <div className="country-item" key={index}>
-              <div className="country-item-header">
-                <div className="country-image"></div>
-                <div className="country-name">{item.cur_nm.split(" ")[0]}</div>
+    <div className="container">
+      <Header
+        leftContent={
+          <div className="exchange-header">
+            <FaChevronLeft
+              style={{ fontSize: "22px" }}
+              onClick={() => setIsToggle(false)}
+            />
+            통화
+          </div>
+        }
+      />
+      <div className="country-list">
+        {exchangeRates.map((item, index) => (
+          <div
+            className="country-item"
+            key={index}
+            onClick={() => handleSelectCountry(item)}
+          >
+            <div className="country-item-header">
+              <div className="country-image">
+                {countryImages[item.cur_unit] && (
+                  <img
+                    className="flag"
+                    src={countryImages[item.cur_unit]}
+                    alt={item.cur_nm}
+                  />
+                )}
               </div>
-              <div className="country-amount">{item.cur_unit}</div>
+              <div className="country-name">{item.cur_nm.split(" ")[0]}</div>
             </div>
-          ))}
-        </div>
+            <div className="country-amount">{item.cur_unit}</div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   ) : (
     <div className="container">
       <Header leftContent={<div className="exchange-header">환율</div>} />
       <div className="last-update">
-        마지막 업데이트 <span className="">{`2024.07.22 14:26`}</span>
+        마지막 업데이트 <span className="">{`${getCurrentTime()} 14:26`}</span>
       </div>
       <div className="ex-rate-list">
-        {defaultRates
+        {defaultRates.length > 0
           ? defaultRates.map((rate) => (
               <ExRateHeaderItem
                 key={rate.cur_unit}
@@ -116,16 +140,24 @@ const ExchangeRatePage = () => {
                 dealBasR={rate.deal_bas_r}
               />
             ))
-          : defaultCurrencies.map((item, index) => (
-              <ExRateHeaderItem curUnit={item} dealBasR="..." />
+          : defaultCurrencies.map((item) => (
+              <ExRateHeaderItem key={item} curUnit={item} dealBasR="..." />
             ))}
       </div>
-      {/* state값에 따라 target설정 */}
       <div className="selected-list">
-        {/* base country */}
         {baseCountry ? (
-          <div onClick={() => setIsToggle(true)}>
-            <SelectedCountry type="base" amount={amount} {...baseCountry} />
+          <div
+            onClick={() => {
+              setIsBase(true);
+              setIsToggle(true);
+            }}
+          >
+            <SelectedCountry
+              type="base"
+              amount={amount}
+              {...baseCountry}
+              countryImage={countryImages[baseCountry.cur_unit]}
+            />
           </div>
         ) : (
           <div className="country-item">
@@ -135,10 +167,19 @@ const ExchangeRatePage = () => {
             </div>
           </div>
         )}
-        {/* target country */}
         {targetCountry ? (
-          <div onClick={() => setIsToggle(true)}>
-            <SelectedCountry type="target" amount={amount} {...targetCountry} />
+          <div
+            onClick={() => {
+              setIsBase(false);
+              setIsToggle(true);
+            }}
+          >
+            <SelectedCountry
+              type="target"
+              amount={amount}
+              {...targetCountry}
+              countryImage={countryImages[targetCountry.cur_unit]}
+            />
           </div>
         ) : (
           <div className="country-item">
