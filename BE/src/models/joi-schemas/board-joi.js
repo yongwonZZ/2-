b7ad5fc3cd7b5d071a4641d2abc1c26c3e3757da
yongwonZZ <shin082508@naver.com>
2024-07-31 23:47:39 +0900

    오류가 너무 많고

--- BE/src/models/joi-schemas/board-joi.js
+++ BE/src/models/joi-schemas/board-joi.js
@@ -1,1 +1,9 @@
 import Joi from 'joi';
+
+export const BoardJoi = Joi.object({
+  userId: Joi.string().required(),
+  boardId: Joi.string().required(),
+  contents: Joi.string().min(1).required(),
+  category: Joi.string().required(),
+  like: Joi.number().default(0),
+});
