105627d15a0a4f806ff84e57e7da6c2d13289bd4
J-yun-ji <qety_1356@naver.com>
2024-08-01 23:49:36 +0900

    Feat: 룩필터 구현

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -5,9 +5,9 @@ import MainPage from "./pages/mainPage/MainPage";
 import AirlineSearchPage from "./pages/airlinePage/AirlineSearchPage";
 import AirlineAllPage from "./pages/airlinePage/AirlineAllPage";
 import ExChangeRatePage from "./pages/exchangeRatePage/ExchangeRatePage";
-import ParkingPage from "./pages/airlineInfoPages/pages/ParkingPage";
-import FacilitiesPage from "./pages/airlineInfoPages/pages/FacilitiesPage";
-import TerminalMapPage from "./pages/airlineInfoPages/pages/TerminalMapPage";
+import ParkingPage from "./pages/airlineInfoPages/Pages/ParkingPage";
+import FacilitiesPage from "./pages/airlineInfoPages/Pages/FacilitiesPage";
+import TerminalMapPage from "./pages/airlineInfoPages/Pages/TerminalMapPage";
 // import BoardingPass from "./pages/boardingPass/BoardingPass";
 import Login from "./pages/login/Login";
 // @ts-ignore
@@ -15,7 +15,7 @@ import MyPage from "./pages/myPage/MyPage";
 import AirportFashion from "./pages/airportFashionMain/airportFashion";
 import LookDetails from "./pages/airportFashionPost/lookDetails";
 import PostUpload from "./pages/airportFashionPost/postUpload/PostUpload";
-import CongestionPage from "./pages/airlineInfoPages/pages/CongestionPage";
+import CongestionPage from "./pages/airlineInfoPages/Pages/CongestionPage";
 import AirlineDetailPage from "./pages/airlinePage/AirlineDetailPage";
 import Navbar from "./components/Navbar";
 // @ts-ignore
@@ -28,31 +28,31 @@ function App() {
   const [ticketCount, setTicketCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className='App'>
       <QueryClientProvider client={queryClient}>
         <Navbar ticketCount={ticketCount} />
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/airline-search" element={<AirlineSearchPage />} />
-          <Route path="/airline-search/:id" element={<AirlineDetailPage />} />
-          <Route path="/exchange" element={<ExChangeRatePage />} />
-          <Route path="/parking" element={<ParkingPage />} />
-          <Route path="/facilities" element={<FacilitiesPage />} />
-          <Route path="/terminalmap" element={<TerminalMapPage />} />
-          <Route path="/airline-all" element={<AirlineAllPage />} />
-          <Route path="/congestion" element={<CongestionPage />} />
+          <Route path='/' element={<MainPage />} />
+          <Route path='/airline-search' element={<AirlineSearchPage />} />
+          <Route path='/airline-search/:id' element={<AirlineDetailPage />} />
+          <Route path='/exchange' element={<ExChangeRatePage />} />
+          <Route path='/parking' element={<ParkingPage />} />
+          <Route path='/facilities' element={<FacilitiesPage />} />
+          <Route path='/terminalmap' element={<TerminalMapPage />} />
+          <Route path='/airline-all' element={<AirlineAllPage />} />
+          <Route path='/congestion' element={<CongestionPage />} />
           {/* <Route
             path="/BoardingPass"
             element={<BoardingPass setTicketCount={setTicketCount} />}
           /> */}
-          <Route path="/Login" element={<Login />} />
-          <Route path="/MyPage" element={<MyPage />} />
-          <Route path="/CreateAccount" element={<CreateAccount />} />
-          <Route path="/airportFashion" element={<AirportFashion />} />
-          <Route path="/lookDetails" element={<LookDetails />} />
-          <Route path="/postUpload" element={<PostUpload />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/MyPage' element={<MyPage />} />
+          <Route path='/CreateAccount' element={<CreateAccount />} />
+          <Route path='/airportFashion' element={<AirportFashion />} />
+          <Route path='/lookDetails' element={<LookDetails />} />
+          <Route path='/postUpload' element={<PostUpload />} />
           <Route
-            path="/boardingPass/:id"
+            path='/boardingPass/:id'
             element={<BoardingPassDetails setTicketCount={setTicketCount} />}
           />
         </Routes>
