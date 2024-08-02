ffa3d6ef42c9f8d0d9d72bd5888944a99c97314d
J-yun-ji <qety_1356@naver.com>
2024-08-02 23:40:19 +0900

    Style: 게시글 및 룩필터

--- FE/src/pages/airportFashionMain/airportFashionComponents/LookFilter.tsx
+++ FE/src/pages/airportFashionMain/airportFashionComponents/LookFilter.tsx
@@ -3,9 +3,13 @@ import "../airportFashion.css";
 
 interface LookFilterProps {
   onFilterClick: (filter: string) => void;
+  selectedFilter: string;
 }
 
-const LookFilter: React.FC<LookFilterProps> = ({ onFilterClick }) => {
+const LookFilter: React.FC<LookFilterProps> = ({
+  onFilterClick,
+  selectedFilter,
+}) => {
   const styles = [
     "",
     "캐주얼",
@@ -20,7 +24,15 @@ const LookFilter: React.FC<LookFilterProps> = ({ onFilterClick }) => {
   return (
     <div className='look-button-container'>
       {styles.map((style) => (
-        <button key={style} onClick={() => onFilterClick(style)}>
+        <button
+          key={style}
+          onClick={() => onFilterClick(style)}
+          style={{
+            backgroundColor:
+              selectedFilter === style ? "rgb(1, 23, 89)" : "initial",
+            color: selectedFilter === style ? "#fff" : "initial",
+          }}
+        >
           {style === "" ? "전체" : style}
         </button>
       ))}
