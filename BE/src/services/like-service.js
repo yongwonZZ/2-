f6cf8d33806406e162af0ca20fa0041fca8538be
yongwonZZ <shin082508@naver.com>
2024-08-04 01:16:45 +0900

    좋아요 기능 쿼리스트링

--- BE/src/services/like-service.js
+++ BE/src/services/like-service.js
@@ -1,35 +1,41 @@
 import asyncHandler from 'express-async-handler';
-import { Like } from '../models/model.js';
+import { Like, Board } from '../models/model.js';
 
-//좋아요 상태 확인
-export const checkLike = asyncHandler(async (req, res) => {
-  const { userId, boardId } = req.body;
+// 좋아요 상태 확인
+export const isLiked = asyncHandler(async (req, res) => {
+  const { userId, boardId } = req.query;
+
+  // 좋아요 여부 확인
   const like = await Like.findOne({ userId, boardId });
-  res.json({ liked: !!like });
-});
 
-//좋아요 수 조회 지워도 된다?
-export const getLikeCount = asyncHandler(async (req, res) => {
-  const { boardId } = req.body;
-  const likeCount = await Like.find({ board: boardId }).countDocuments();
-  res.json({ likeCount });
-});
-
-// 좋아요 (누르면 보드 스키마에 좋아요 수 +1)
-export const like = asyncHandler(async (req, res) => {
-  const { userId, boardId } = req.body;
-  const like = await Like.findOne({ userId, boardId });
+  // 좋아요 여부 상태 반환
   if (like) {
-    await like.create({ userId, boardId });
+    return res.json({ liked: true });
+  } else {
+    return res.json({ liked: false });
   }
-  const likeCount = await Like.find({ boardId }).countDocuments();
-  res.json({ liked: true, likeCount }); //반대, 카운트 필요없다
 });
 
-// 좋아요 취소
-export const unlike = asyncHandler(async (req, res) => {
-  const { userId, boardId } = req.body;
-  await Like.findOneAndDelete({ userId, boardId });
-  const likeCount = await Like.find({ boardId }).countDocuments();
-  res.json({ liked: false, likeCount }); //반대, 카운트 필요없다
+// 좋아요 추가 및 취소
+export const toggleLike = asyncHandler(async (req, res) => {
+  const { userId, boardId } = req.query;
+
+  // 좋아요가 이미 존재하는지 확인
+  const like = await Like.findOne({ userId, boardId });
+
+  if (like) {
+    // 좋아요가 존재하면 삭제 (좋아요 취소)
+    await Like.deleteOne({ userId, boardId });
+    await Board.findByIdAndUpdate(boardId, { $inc: { like: -1 } });
+
+    // 좋아요 취소 상태 반환
+    return res.json({ liked: false });
+  } else {
+    // 좋아요가 존재하지 않으면 새로 생성 (좋아요 추가)
+    await Like.create({ userId, boardId });
+    await Board.findByIdAndUpdate(boardId, { $inc: { like: 1 } });
+
+    // 좋아요 추가 상태 반환
+    return res.json({ liked: true });
+  }
 });
