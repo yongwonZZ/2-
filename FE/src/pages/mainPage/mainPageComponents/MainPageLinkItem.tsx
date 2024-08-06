2bb402ef9790650f32bfc66ea9e2b55107030ff1
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-08-07 02:21:50 +0900

    Feat: 메인페이지 UI 개선

--- FE/src/pages/mainPage/mainPageComponents/MainPageLinkItem.tsx
+++ FE/src/pages/mainPage/mainPageComponents/MainPageLinkItem.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styles from "../../../styles/mainPage/MainPageLinkItem.module.css";
-import { GrNext } from "react-icons/gr";
+import { motion } from "framer-motion";
 
 type MainPageLinkItemProps = {
   icon: React.ReactNode;
@@ -8,17 +8,29 @@ type MainPageLinkItemProps = {
   navigateTo: string;
 };
 
+const containerVariants = {
+  hidden: { opacity: 0, x: "-100vw" },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 0.5, ease: "easeInOut" },
+  },
+};
+
 function MainPageLinkItem({ icon, title, navigateTo }: MainPageLinkItemProps) {
   const navigate = useNavigate();
 
   return (
-    <div className={styles["link-item"]} onClick={() => navigate(navigateTo)}>
-      <div>
-        {icon}
-        {title}
-      </div>
-      <GrNext />
-    </div>
+    <motion.div
+      className={styles["link-item"]}
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+      onClick={() => navigate(navigateTo)}
+    >
+      <div style={{ width: "80%" }}>{icon}</div>
+      <p className={styles.title}>{title}</p>
+    </motion.div>
   );
 }
 
