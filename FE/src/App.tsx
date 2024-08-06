2a9d2fafd58277f57826c8f96039092ffc0b4c87
lim12749 <50003828+lim12749@users.noreply.github.com>
2024-08-06 21:44:57 +0900

    경로 수정

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,23 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import MainPage from "./pages/mainPage/MainPage";
-import AirlineSearchPage from "./pages/airlinePage/AirlineSearchPage";
-import AirlineAllPage from "./pages/airlinePage/AirlineAllPage";
 import ExChangeRatePage from "./pages/exchangeRatePage/ExchangeRatePage";
-import ParkingPage from "./pages/airlineInfoPages/pages/ParkingPage";
-import FacilitiesPage from "./pages/airlineInfoPages/pages/FacilitiesPage";
-import TerminalMapPage from "./pages/airlineInfoPages/pages/TerminalMapPage";
 import AirportFashion from "./pages/airportFashionMain/AirportFashion";
 import LookDetails from "./pages/airportFashionMain/LookDetails";
 import PostUpload from "./pages/airportFashionMain/postUpload/PostUpload";
-import CongestionPage from "./pages/airlineInfoPages/pages/CongestionPage";
-import AirlineDetailPage from "./pages/airlinePage/AirlineDetailPage";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import { PageNames } from "./utils/PageNames"; // Enum import
 import LoginMain from './pages/login/LoginMain'; // loginMain import
 import BoardingPassMain from './pages/boardingPassMain/BoardingPassMain'; // boardingPassMain import
+import AirPortMain from './pages/airPortsMain/AirPortMain'; // airPortMain import
 
 const queryClient = new QueryClient();
 
@@ -50,15 +43,10 @@ function App() {
               rightContent={<button onClick={() => navigate('/help')}>도움말</button>}
           />
           <Routes>
-            <Route path='/' element={<MainPage />} />
-            <Route path='/airline-search' element={<AirlineSearchPage />} />
-            <Route path='/airline-search/:id' element={<AirlineDetailPage />} />
+            {/* 공항 관련 라우트 */}
+            <Route path='/*' element={<AirPortMain />} />
+
             <Route path='/exchange' element={<ExChangeRatePage />} />
-            <Route path='/parking' element={<ParkingPage />} />
-            <Route path='/facilities' element={<FacilitiesPage />} />
-            <Route path='/terminalmap' element={<TerminalMapPage />} />
-            <Route path='/airline-all' element={<AirlineAllPage />} />
-            <Route path='/congestion' element={<CongestionPage />} />
             <Route path='/airportFashion' element={<AirportFashion />} />
             <Route path='/lookDetails' element={<LookDetails />} />
             <Route path='/postUpload' element={<PostUpload />} />
