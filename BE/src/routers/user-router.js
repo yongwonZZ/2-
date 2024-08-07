9e4313c965bc60c3fcab3dba453bc12de7a44520
J-yun-ji <qety_1356@naver.com>
2024-08-08 00:52:58 +0900

    커밋0052

--- BE/src/routers/user-router.js
+++ BE/src/routers/user-router.js
@@ -6,17 +6,28 @@ import {
   getUser,
   resignUser,
 } from '../services/user-service.js';
-import permission from '../middlewares/permission.js'; // 유저인증 & 권한 체크
+import { permission, validate } from '../middlewares/index.js';
+import { UpdateUserJoi } from '../models/joi-schemas/user-joi.js';
 
 const router = express.Router();
 
 router.get('/mypage', permission('user'), getUser); // 회원 정보 조회
-router.put('/mypage/:id', permission('user'), updateUser); // 회원 정보 수정
+router.put(
+  '/mypage/:id',
+  permission('user'),
+  validate(UpdateUserJoi),
+  updateUser
+); // 회원 정보 수정
 router.delete('/mypage/:id', permission('user'), resignUser); // 회원 삭제(탈퇴)
 
 router.get('/admin', permission('admin'), getUserList); // 전체 회원 목록 조회
 router.get('/admin/:id', permission('admin'), getUser); // 회원 정보 조회
-router.put('/admin/:id', permission('admin'), updateUser); // 회원 정보 수정
+router.put(
+  '/admin/:id',
+  permission('admin'),
+  validate(UpdateUserJoi),
+  updateUser
+); // 회원 정보 수정
 router.delete('/admin/:id', permission('admin'), deleteUser); // 회원 삭제
 
 export default router;
