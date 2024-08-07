8bfb602c23fe714aac13130659fea546a5a0bea6
G-jjunny <rudwns9551@naver.com>
2024-08-07 16:13:33 +0900

    css복구

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -7,9 +7,9 @@ import LookDetails from "./pages/airportFashionMain/LookDetails";
 import PostUpload from "./pages/airportFashionMain/postUpload/PostUpload";
 import Navbar from "./components/Navbar";
 import { PageNames } from "./utils/PageNames"; // Enum import
-import LoginMain from './pages/login/LoginMain'; // loginMain import
-import BoardingPassMain from './pages/boardingPassMain/BoardingPassMain'; // boardingPassMain import
-import AirPortMain from './pages/airPortsMain/AirPortMain'; // airPortMain import
+import LoginMain from "./pages/login/LoginMain"; // loginMain import
+import BoardingPassMain from "./pages/boardingPassMain/BoardingPassMain"; // boardingPassMain import
+import AirPortMain from "./pages/airPortsMain/AirPortMain"; // airPortMain import
 
 const queryClient = new QueryClient();
 
@@ -20,25 +20,25 @@ function App() {
   // 현재 경로에 따른 페이지 이름을 반환하는 함수
   const getPageName = (path: string) => {
     switch (path) {
-      case '/':
-        return '공항'; // PageNames.MAIN;
-      case '/exchange':
-        return '환율';
-      case '/airportFashion':
-        return '공항패션';
-      case '/login':
-        return '로그인';
-      case '/login/myPage':
-        return '마이페이지';
-      case '/boardingPass':
-        return '내 티켓';
-      case '/postUpload':
-        return '포스트 업로드';
-      case '/lookDetails':
-        return '룩 디테일';
-        // 필요한 다른 경로들도 추가합니다.
+      case "/":
+        return "공항"; // PageNames.MAIN;
+      case "/exchange":
+        return "환율";
+      case "/airportFashion":
+        return "공항패션";
+      case "/login":
+        return "로그인";
+      case "/login/myPage":
+        return "마이페이지";
+      case "/boardingPass":
+        return "내 티켓";
+      case "/postUpload":
+        return "포스트 업로드";
+      case "/lookDetails":
+        return "룩 디테일";
+      // 필요한 다른 경로들도 추가합니다.
       default:
-        return '사이트 이름'; // 기본 타이틀 설정
+        return "사이트 이름"; // 기본 타이틀 설정
     }
   };
 
@@ -48,23 +48,23 @@ function App() {
   }, [location.pathname]);
 
   return (
-      <div className='App'>
-        <QueryClientProvider client={queryClient}>
-          <Routes>
-            {/* 공항 관련 라우트 */}
-            <Route path="/*" element={<AirPortMain />} />
-            <Route path="/airportFashion" element={<AirportFashion />} />
-            <Route path="/lookDetails" element={<LookDetails />} />
-            <Route path="/postUpload" element={<PostUpload />} />
-
-            {/* 로그인 및 마이페이지 관련 라우트 */}
-            <Route path="/login/*" element={<LoginMain />} />
-            {/* BoardingPass 관련 라우트 */}
-            <Route path="/boardingPass/*" element={<BoardingPassMain />} />
-          </Routes>
-          <Navbar />
-        </QueryClientProvider>
-      </div>
+    <div className="App">
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          {/* 공항 관련 라우트 */}
+          <Route path="/*" element={<AirPortMain />} />
+          <Route path="/airportFashion" element={<AirportFashion />} />
+          <Route path="/lookDetails" element={<LookDetails />} />
+          <Route path="/postUpload" element={<PostUpload />} />
+          <Route path="/exchange" element={<ExChangeRatePage />} />
+          {/* 로그인 및 마이페이지 관련 라우트 */}
+          <Route path="/login/*" element={<LoginMain />} />
+          {/* BoardingPass 관련 라우트 */}
+          <Route path="/boardingPass/*" element={<BoardingPassMain />} />
+        </Routes>
+        <Navbar />
+      </QueryClientProvider>
+    </div>
   );
 }
 
