bebf6b60f5891df00a98e60aba26747f6acada5b
yongwonZZ <shin082508@naver.com>
2024-08-07 18:44:06 +0900

    기능완성 유저기능 업데이트 필요

--- BE/src/services/like-service.js
+++ BE/src/services/like-service.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import { Like, Board } from '../models/model.js';
+import mongoose from 'mongoose';
 
 // 좋아요 상태 확인
 export const isLiked = asyncHandler(async (req, res) => {
@@ -18,22 +19,41 @@ export const isLiked = asyncHandler(async (req, res) => {
 
 // 좋아요 추가 및 취소
 export const toggleLike = asyncHandler(async (req, res) => {
-  const { likeId } = req.body;
+  const { userId, boardId } = req.body;
 
-  // likeId를 사용하여 좋아요 찾기
-  const like = await Like.findById(likeId);
+  // boardId가 유효한 ObjectId인지 확인
+  if (!mongoose.Types.ObjectId.isValid(boardId)) {
+    return res.status(400).json({ message: '유효하지 않은 boardId입니다.' });
+  }
+
+  const boardObjectId = new mongoose.Types.ObjectId(boardId);
+
+  // userId와 boardId를 사용하여 좋아요 찾기
+  const like = await Like.findOne({ userId, boardId: boardObjectId });
 
   if (like) {
     // 좋아요가 존재하면 삭제 (좋아요 취소)
-    await Like.deleteOne({ _id: likeId });
-    await Board.findByIdAndUpdate(like.boardId, { $inc: { like: -1 } });
+    await Like.deleteOne({ _id: like._id });
+    const updatedBoard = await Board.findByIdAndUpdate(
+      boardObjectId,
+      { $inc: { like: -1 } },
+      { new: true }
+    );
+
+    console.log(`좋아요 취소: ${updatedBoard.like}`);
 
     // 좋아요 취소 상태 반환
     return res.json({ liked: false });
   } else {
     // 좋아요가 존재하지 않으면 새로 생성 (좋아요 추가)
-    const newLike = await Like.create({ userId, boardId });
-    await Board.findByIdAndUpdate(newLike.boardId, { $inc: { like: 1 } });
+    const newLike = await Like.create({ userId, boardId: boardObjectId });
+    const updatedBoard = await Board.findByIdAndUpdate(
+      boardObjectId,
+      { $inc: { like: 1 } },
+      { new: true }
+    );
+
+    console.log(`좋아요 추가: ${updatedBoard.like}`);
 
     // 좋아요 추가 상태 반환
     return res.json({ liked: true });
